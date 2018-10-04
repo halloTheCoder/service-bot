b68c4cf2aec24b2761f631ee8cbef7c298d755ee
Akash Chandra <akash.chandra8d@gmail.com>
2018-10-05 02:23:06 +0530

    Add run_app.py

---
+++ run_app.py
@@ -0,0 +1,15 @@
+from rasa_core.channels.slack import SlackInput
+from rasa_core.agent import Agent
+from rasa_core.interpreter import RasaNLUInterpreter
+import yaml
+from rasa_core.utils import EndpointConfig
+
+
+nlu_interpreter = RasaNLUInterpreter('./models/nlu_tf/default/service_nlu')
+action_endpoint = EndpointConfig(url="http://localhost:5055/webhook")
+agent = Agent.load('./models/dialogue', interpreter = nlu_interpreter, action_endpoint = action_endpoint)
+
+input_channel = SlackInput('xoxb-446873191671-445226503153-NDkcQJudWyXwTrxiLn1WT4dj' #your bot user authentication token
+                           )
+
+agent.handle_channels([input_channel], 5004, serve_forever=True)
